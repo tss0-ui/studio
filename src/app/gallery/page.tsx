40b6950a19c544597ab086e034afd3909a4d421f
Travis Gordon <travis.gordon727@gmail.com>
2025-05-21 15:24:09 +0000

    gallary and vscode settings

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,28 @@
+<div className="row">
+  <div className="column">
+    <img src="https://cdn-thumbs.imagevenue.com/09/57/a7/ME1ASDMM_t.jpg" alt="" />
+
+    <img src="https://cdn-thumbs.imagevenue.com/65/9b/44/ME1ASDMQ_t.jpg" alt="" />
+
+    <img src="https://cdn-thumbs.imagevenue.com/72/41/e1/ME1ASDMS_t.jpg" alt="" />
+
+    <img src="https://cdn-thumbs.imagevenue.com/4d/5b/15/ME1ASDMU_t.jpg" alt="" />
+  </div>
+  
+  <div className="column">
+    <img src="https://cdn-thumbs.imagevenue.com/e4/e3/a9/ME1ASDMV_t.jpg" alt="" />
+
+    <img src="https://cdn-thumbs.imagevenue.com/a7/75/c3/ME1ASDMY_t.jpg" alt="" />
+
+    <img src="https://cdn-thumbs.imagevenue.com/74/7e/a3/ME1ASDMZ_t.jpg" alt="" />
+  </div>
+  <div className="column">
+    <img src="https://cdn-thumbs.imagevenue.com/ea/52/7b/ME1ASDN2_t.jpg" alt="" />
+
+    <img src="https://cdn-thumbs.imagevenue.com/38/31/47/ME1ASDN4_t.jpg" alt="" />
+
+    <img src="https://cdn-thumbs.imagevenue.com/24/ea/14/ME1ASDN9_t.jpg" alt="" />
+
+    <img src="https://cdn-thumbs.imagevenue.com/34/c8/79/ME1ASDND_t.png" alt="" />
+  </div>
+</div>
